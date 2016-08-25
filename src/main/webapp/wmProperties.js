9d264b207cc250256c9aedce8cc85a15f5fa6581
root <root@38c4157810bd>
2016-08-25 05:40:22 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "verifyVariableRename",
+  "homePage" : "Main",
+  "name" : "verifyVariableRename",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
