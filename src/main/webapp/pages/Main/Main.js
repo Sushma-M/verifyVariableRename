9f11b8d651a9f3ccaaf3edc5a6adef2d3f7e38de
sushma m null <sushma.maddirala+3@wavemaker.com>
2016-08-25 08:56:04 +0000

    New files added - [demo-options.json,testppage.variables.json,servicedef.xml,User.java,googleapis_connection_settings.json,hrdb_procedure.json,DepartmentService.java,DepartmentServiceImpl.java,UserService.java,UserServiceImpl.java,general-options.json,hrdb-queries.hbm.xml,googleapis.spring.xml,VacationController.java,googleapis_apiTarget.json,VacationDao.java,testpartial1.html,testppage.html,googleapis_API_REST_SERVICE.json,googleapis_apiTarget.json,hrdb-procedures.mappings.json,DepartmentDao.java,ProcedureExecutionController.java,securityService.spring.xml,db-connection-settings.json,VacationServiceImpl.java,intercept-urls.json,HrdbProcedureExecutorServiceImpl.java,Vacation.java,EmployeeServiceImpl.java,testppage.css,Department.java,HrdbQueryExecutorService.java,testppage.js,servicedef.xml,hrdb.properties,DepartmentController.java,hrdb.log,Employee.java,hrdb_published_dataModel.json,package-info.java,testpartial1.css,hrdb_query.json,hrdb.spring.xml,UserController.java,HrdbProcedureExecutorService.java,EmployeeDao.java,hrdb.script,QueryExecutionController.java,roles.json,testpartial1.variables.json,EmployeeService.java,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,UserDao.java,EmployeeController.java,servicedef.xml,testpartial1.js,testppage,testpartial1] Files modified - [pages-config.json,Main.html,footer.variables.json,rightnav.variables.json,project-services.xml,Common.variables.json,project-security-provider.xml,.wmproject.properties,Login.variables.json,app.variables.json,topnav.variables.json,Main.variables.json,leftnav.variables.json,production.properties,header.variables.json,Main.js,Common.html,Login.html,topnav.html,pom.xml,index.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,17 +1,48 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
     };
 
+
+    $scope.button1Click = function($event, $isolateScope) {
+        $scope.Widgets.dialog1.open();
+    };
+
+
+    /* $scope.button4Click = function($event, $isolateScope) {
+               $scope.Widgets.popover1.open(); */
+};
+
 }]);
+
+Application.$controller("dialog1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("grid1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("liveform1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
